99a20f310e37c267863e33fb5f12b43c590951b3
Maxwell Da Silva <dayvson@gmail.com>
2015-03-12 17:40:00 -0400

    Initial Commit

---
+++ HeadphoneBluetoothSpotify/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HeadphoneBluetoothSpotify
+//
+//  Created by Dasilva, Maxwell on 3/12/15.
+//  Copyright (c) 2015 maxwell dasilva. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
